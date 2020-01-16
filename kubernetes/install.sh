4d458276eaa08c348668bb6baac787bc1454cfa0
hlc-369123 <31560195+hlc-369123@users.noreply.github.com>
2020-01-16 16:53:43 +0800

    Update install.sh

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -2,10 +2,24 @@ bash -x copy_ssh_id.sh >>/opt/install_k8s.log 2>&1
 
 bash -x create_ssl.sh >>/opt/install_k8s.log 2>&1
 
-dep_kubectl_tool.sh
+bash -x dep_kubectl_tool.sh >>/opt/install_k8s.log 2>&1
 
-dep_etcd.sh
+修改etcd ip，ip不要写错，不然etcd无法启动
+bash -x dep_etcd.sh >>/opt/install_k8s.log 2>&1
 
-dep_flanneld.sh
+bash -x dep_flanneld.sh >>/opt/install_k8s.log 2>&1
 
-kube-apiserver_nginx_proxy.sh
+修改etcd ip，ip不要写错
+bash -x kube-apiserver_nginx_proxy.sh >>/opt/install_k8s.log 2>&1
+
+bash -x dep_master.sh >>/opt/install_k8s.log 2>&1
+
+bash -x kube-controller-manager.sh >>/opt/install_k8s.log 2>&1
+
+bash -x kube-scheduler.sh >>/opt/install_k8s.log 2>&1
+
+bash -x dep_docker.sh >>/opt/install_k8s.log 2>&1
+
+bash -x kubelet.sh >>/opt/install_k8s.log 2>&1
+
+bash -x kube-proxy.sh >>/opt/install_k8s.log 2>&1
