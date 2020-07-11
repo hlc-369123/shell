c515c91e8fde215b065def4341de9294b050557d
hlc-369123 <31560195+hlc-369123@users.noreply.github.com>
2020-07-11 22:47:10 +0800

    Create switch_stst_es.sh

---
+++ switch_ES/switch_stst_es.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+set -e -o pipefail
+
+ssh_port=22
+SSH="ssh -n -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p ${ssh_port}"
+
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S')" "[INFO]" "$@"
+}
+
+warnlog() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S')" "[WARN]" "$@"
+}
+
+host_ips=$(xms-manage db list 2>/dev/null | awk '{print $1}')
+if [[ -z "${host_ips}" ]]; then
+  warnlog "Failed to get admin host ips"
+fi
+log "Get admin host ips:"
+echo "$host_ips"
+
+show_usage() {
+  echo -e "Usage :\n \n--user \t\t\tuser name [\$XMS_USERNAME] \n--password \t\tuser password [\$XMS_PASSWORD] \n--action \t\t<disable/enable>\n"
+  echo -e "\ne.g : \n\n$0 --user [\$XMS_USERNAME] --password [\$XMS_PASSWORD] --action <disable/enable>\n"
+}
+
+userinfo() {
+  local user_par=$1
+  local userinfo=$2
+  echo "${user_par}" | awk -F "${userinfo}" '{print $2}' | awk '{print $1}'
+}
+
+check_self_login() {
+  local local_admin_ip=$1
+  $SSH "${local_admin_ip}" hostname &>/dev/null
+  if [[ $? -ne 0 ]]; then
+    warnlog "${local_admin_ip} ssh failed !..." && exit 1
+  fi
+}
+
+xms_cli_use() {
+  local action=$1
+  if [[ -n "${user}" && -n "${password}" ]]; then
+    xms-cli --user "${user}" --password "${password}" ${action}
+  else
+    xms-cli ${action}
+  fi
+}
+
+es_action() {
+  local action=$1
+  local enabled=$2
+  if [[ -n "${enabled}" ]]; then enabled="--enabled=$enabled"; fi
+  xms_cli_use "-l cluster $action elasticsearch $enabled"
+}
+
+check_xms_stat() {
+  local ip=$1
+  local service_name=$2
+  local node_id=$(xms_cli_use "host list" | grep "${ip}" | awk '{print $2}')
+  local node_roles=$(xms_cli_use "host show ${node_id}" | grep "roles" | awk '{print $4}')
+  for i in {0..9}; do
+    if [[ "${service_name}" == "Postgres_xmsd" ]]; then
+      local active_stat=$(xms_cli_use "host show ${node_id}" | awk '/ status/{print $4}')
+      if [[ "${i}" -eq 0 && "${active_stat}" == warning ]]; then
+        xms_cli_use "host set --roles ${node_roles} ${node_id}" 1>/dev/null
+      fi
+    elif [[ "${service_name}" == "action_status" ]]; then
+      local active_stat=$(xms_cli_use "host show ${node_id}" | grep action_status | awk '{print $4}')
+      if [[ "${active_stat}" != "updating_host_roles" ]]; then active_stat="active"; fi
+    fi
+    local up_stat=$(xms_cli_use "host show ${node_id}" | awk '/up /{print $4}')
+    if [[ "${active_stat}" == "active" && "${up_stat}" == "true" ]]; then
+      break
+    elif [[ "${i}" == '9' && "${active_stat}" != "active" ]]; then
+      warnlog "$service_name status is $active_stat，Please check it！..." && exit 1
+    else
+      sleep 20
+    fi
+  done
+}
+
+refresh_the_role() {
+  local host_ips=$1
+  for ip in ${host_ips}; do
+    check_self_login "${ip}"
+    log "restart of xmsd on $ip..."
+    $SSH "${ip}" systemctl restart xmsd.service 2>/dev/null
+    local xmsd_status=$($SSH "${ip}" systemctl is-active xmsd.service 2>/dev/null)
+    if [[ "${xmsd_status}" != 'active' ]]; then sleep 10; fi
+  done
+  for ip in ${host_ips}; do
+    local xmsd_status=$($SSH "${ip}" systemctl is-active xmsd.service 2>/dev/null)
+    if [[ "${xmsd_status}" == 'active' ]]; then
+      log "$ip xmsd is active,Refresh the role..."
+      local node_id=$(xms_cli_use "host list" | grep "${ip}" | awk '{print $2}')
+      local node_roles=$(xms_cli_use "host show ${node_id}" | grep "roles" | awk '{print $4}')
+      check_xms_stat "${ip}" "Postgres_xmsd"
+      xms_cli_use "host set --roles ${node_roles} ${node_id}" 1>/dev/null
+      check_xms_stat "${ip}" "action_status"
+    else
+      warnlog "$ip Xmsd  state failed, Please check it !..."
+      systemctl status xmsd.service && exit 1
+    fi
+  done
+}
+
+action_es() {
+  local action=$1
+  es_action set "${action}" 1>/dev/null
+  if [[ "${action}" == "false" ]]; then stst_action="true"; else stst_action="false"; fi
+  for i in {0..9}; do
+    es_stat=$(es_action show | grep elasticsearch_enabled | awk '{print $4}')
+    true_num=1
+    if ! xms-manage db trigger list 2>/dev/null | grep -o "${stst_action}" &>/dev/null; then true_num=0; fi
+    if [[ "${es_stat}" == "${action}" && "${true_num}" == '0' ]]; then
+      refresh_the_role "${host_ips}"
+      refresh_code=$?
+      if ! xms_cli_use 'service list' | grep 'elasticsearch' | grep 'error' && [[ "${refresh_code}" -eq 0 ]]; then
+        log "ES $action successful !..." && exit 0
+      else
+        warnlog "The nodes below ES failed to $action，Please try again ！..."
+        xms_cli_use "service list" | grep 'elasticsearch' | grep 'error' | awk '{print $14}' && exit 1
+      fi
+    elif [[ $i -eq 9 && "${es_stat}" == "${action}" && "${true_num}" == '0' ]]; then
+      warnlog "ES $action failed,Please try again !..." && exit 1
+    else
+      sleep 20
+    fi
+  done
+}
+
+if [ "$1" == "-h" ]; then
+  show_usage
+  exit 0
+elif [ -z "${XMS_USERNAME}" ]; then
+  if [ "$1" == "-h" ]; then
+    show_usage
+    exit 0
+  elif [ $# -lt 6 -o $# -gt 6 ]; then
+    show_usage
+    exit 0
+  else
+    user=$(userinfo "$*" --user)
+    password=$(userinfo "$*" --password)
+    action=$(userinfo "$*" --action)
+    if ! xms_cli_use "host list" &>/dev/null; then warnlog "Wrong information..." && exit 1; fi
+  fi
+elif [ -n "${XMS_USERNAME}" -o $# -eq 6 ]; then
+  user=$(userinfo "$*" --user)
+  password=$(userinfo "$*" --password)
+  action=$(userinfo "$*" --action)
+  if ! xms_cli_use "host list" &>/dev/null; then warnlog "Wrong information..." && exit 1; fi
+elif [ $# -lt 2 -o $# -gt 2 ]; then
+  show_usage
+  exit 0
+else
+  action=$1
+  if ! xms_cli_use "host list" &>/dev/null; then warnlog "Wrong information..." && exit 1; fi
+fi
+
+if [ "${action}" == disable ]; then
+  action_es false
+elif [ "${action}" == enable ]; then
+  action_es true
+else
+  show_usage
+  exit 0
+fi
